d6568e5d094fb30ddd4c5f622281ce48f25bbede
Ian Calloway <iccalloway92@gmail.com>
2019-03-14 08:37:34 -0400

    Spline predictions written line-by-line

--- track.py
+++ track.py
@@ -16,7 +16,6 @@ import imageio
 import matplotlib.pyplot as plt
 import os
 
-
 # please specify the path to input and output files
 path_to_video = r'C:\Users\zhuji\Videos\src\demo\demo_video.mp4'
 
@@ -42,30 +41,36 @@ N = 1
 # set the boundary for cropping
 boundary = [97,362,95,507]
 
+
 # get splines
-for index, img in enumerate(video):
-    t0 = time.time()
-    pred = cp.get_spline(model,img,output_size=[265,412],preprocessing=True,boundary=boundary)
-    s = cp.interpolate_tongue_spline(pred,points=100)
-    
-    # convert the coordinate back to fit the original image
-    s[:,0]=s[:,0]+boundary[2]
-    s[:,1]=s[:,1]+boundary[0]
-    
-    #ac = cp.get_active_contour(s,img,smooth=False)
-    spline = pd.DataFrame(s,columns=['x','y'])
-    spline['uniqueframe'] = pd.Series([index for i in range(100)])
-    cnn_prediction = cnn_prediction.append(spline)
-    cnn_prediction.to_csv(path_to_csv_output)
-    
-    # plot every Nth frame for inspection
-    if index%N == 0:
-        fig, ax = plt.subplots(figsize=(9, 5))
-        ax.imshow(img, cmap=plt.cm.gray)
-        #ax.plot(three['xcoord'], three['ycoord'], '--r', lw=3)
-        ax.plot(s[:,0], s[:,1], 'ro', lw=3)
-        fig.savefig(path_to_figures+str(index)+'.jpg')
-        plt.clf()  
-        plt.close() 
-    t1 = time.time()
-    print((t1-t0))    
+with open(path_to_csv_output, "w") as csvfile:
+	cnn_prediction.to_csv(csvfile, mode="a", header=True, line_terminator="\n")
+	for index, img in enumerate(video):
+		t0 = time.time()
+		pred = cp.get_spline(model,img,output_size=[265,412],preprocessing=True,boundary=boundary)
+		s = cp.interpolate_tongue_spline(pred,points=100)
+		
+		# convert the coordinate back to fit the original image
+		s[:,0]=s[:,0]+boundary[2]
+		s[:,1]=s[:,1]+boundary[0]
+		
+		#ac = cp.get_active_contour(s,img,smooth=False)
+		
+		spline = pd.DataFrame(s,columns=['x','y'])
+		spline['uniqueframe'] = pd.Series([index for i in range(100)])
+		
+		##Write to csv line by line	-Ian
+		temp_prediction = cnn_prediction.append(spline)
+		temp_prediction.to_csv(csvfile, mode="a", header=False, line_terminator="\n")
+		
+		# plot every Nth frame for inspection
+		if index%N == 0:
+			fig, ax = plt.subplots(figsize=(9, 5))
+			ax.imshow(img, cmap=plt.cm.gray)
+			#ax.plot(three['xcoord'], three['ycoord'], '--r', lw=3)
+			ax.plot(s[:,0], s[:,1], 'ro', lw=3)
+			fig.savefig(path_to_figures+str(index)+'.jpg')
+			plt.clf()  
+			plt.close() 
+		t1 = time.time()
+		print((t1-t0))    
